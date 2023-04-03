1ba75945962ae82615ad63abcb7f70c0b26ec960
s095339 <78518884+s095339@users.noreply.github.com>
2023-04-03 17:09:06 +0800

    spectrogram_finished

--- hw1/hw1_1.py
+++ hw1/hw1_1.py
@@ -4,48 +4,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
 
-def Spectrogram_1_1(sound_flie_name):
-    #load audio file-------------------------------------
+def main():
+    
+    #(a)---------------------------------------------
+    sound_flie_name = "mixkit-losing-piano-2024.wav"
+    print(f"load audiofile {sound_flie_name}...")
     audio, sr = librosa.load(sound_flie_name)
+     #load audio file-------------------------------------
     duration = librosa.get_duration(y=audio,sr=sr)
-    print(f"load audiofile {sound_flie_name}")
     print(f"sampling rate = {sr}, duration = {duration}")
     #----------------------------------------------------
 
     #show plt----------------------------------------------------
     plt.figure()
     hop_len = 512
-    D = librosa.amplitude_to_db(np.abs(librosa.stft(audio, hop_length=hop_len)),
+    window_len = 2048
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(audio, hop_length=hop_len,win_length=window_len)),
                             ref=np.max)
     display.specshow(D, y_axis='log', sr=sr, hop_length=hop_len,
                          x_axis='time')
     plt.show()
     #----------------------------------------------------
-    return
-def WriteChirpToFile_1_c():
-    sr = 8000
-    t = np.arange(0,10,1/sr)
+    
+    #(b)--------------------------------------------
+    t_x1 = np.arange(0,10,hop_len*(1/SR))
+    IF_x1 = 2000*t_x1/np.pi
+    #------------------------------------------------
+    #(c)---------------------------------------------
+    SR = 8000
+    t = np.arange(0,10,1/SR)
     x1 = np.sin(2000*t*t)
     x2 = np.sin(2000*t+10*np.sin(2.5*t*t))
-    sf.write("x1.wav",x1,sr)
-    sf.write("x2.wav",x2,sr)
-    return
-def main():
-    #HW1_1=============================================#
-    #(a)---------------------------------------------
-    sound_flie_name = "mixkit-losing-piano-2024.wav"
-    Spectrogram_1_1(sound_flie_name)
-    #------------------------------------------------
-    #(b)
-
-    #(c)---------------------------------------------
-    WriteChirpToFile_1_c()
+    import os
+    if not os.path.exists("x1.wav"):
+        sf.write("x1.wav",x1,sr)
+    if not os.path.exists("x2.wav"):
+        sf.write("x2.wav",x2,sr)
     #------------------------------------------------
     
     #(d)---------------------------------------------
-    
+    plt.subplot(2,1,1)
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(x1, hop_length=hop_len,win_length=window_len)),
+                            ref=np.max)
+    display.specshow(D, y_axis='log', sr=SR, hop_length=hop_len,
+                         x_axis='time',fmax=8192)
+    t_x1 = np.arange(0,10,hop_len*(1/SR))
+    IF_x1 = 2000*t_x1/np.pi
+    line1 = plt.plot(t_x1,IF_x1)
+    plt.subplot(2,1,2)
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(x2, hop_length=hop_len,win_length=window_len)),
+                            ref=np.max)
+    display.specshow(D, y_axis='log', sr=SR, hop_length=hop_len,
+                         x_axis='time',fmax=8192)
+    #line1 = plt.plot(D)
+    plt.show()
     #------------------------------------------------
-    #================================================#
+
+    
 
 
 
